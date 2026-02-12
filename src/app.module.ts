7502ae2f17b6d3b298f6eaefa8bef7fcd256b2dc
Breno Marques <brenomarques516@gmail.com>
2026-02-12 19:54:08 -0300

    :truck: chore: add url module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UrlModule } from './modules/url/url.module';
 
 @Module({
-  imports: [],
+  imports: [UrlModule],
   controllers: [],
   providers: [],
 })
