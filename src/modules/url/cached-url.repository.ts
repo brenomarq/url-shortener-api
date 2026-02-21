e9dfb80cbe59665eec92c9bc91f6503cbbe20f96
Breno Marques <brenomarques516@gmail.com>
2026-02-21 12:56:53 -0300

    :construction: feat: finish cached url repository logic

--- src/modules/url/cached-url.repository.ts
+++ src/modules/url/cached-url.repository.ts
@@ -1,15 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaUrlRepository } from './prisma-url.repository';
 import { RedisService } from 'src/infrastructure/redis/redis.service';
 import { IUrlRepository } from './interfaces/url.repository.interface';
 import { Url } from './entities/url.entity';
+import { UrlCacheMapper } from './mappers/url-cache.mapper';
 
 @Injectable()
-export class CachedUrlRepository {
+export class CachedUrlRepository implements IUrlRepository {
   private readonly CACHE_TTL_SECONDS = 3600;
 
   constructor(
     private readonly urlRepository: PrismaUrlRepository,
     private readonly redisService: RedisService,
   ) {}
+
+  async save(shortCode: string, originalUrl: string): Promise<Url> {
+    const url = await this.urlRepository.save(shortCode, originalUrl);
+    const urlToCache = UrlCacheMapper.toCache(url);
+    await this.redisService.set(shortCode, urlToCache, this.CACHE_TTL_SECONDS);
+    return url;
+  }
+
+  async findByShortCode(shortCode: string): Promise<Url | null> {
+    const cachedUrl = await this.redisService.get(shortCode);
+
+    if (cachedUrl) {
+      const domainUrl = UrlCacheMapper.toDomain(cachedUrl);
+      return domainUrl;
+    }
+
+    const url = await this.urlRepository.findByShortCode(shortCode);
+    if (!url) throw new NotFoundException('Url not found');
+
+    return url;
+  }
+
+  async incrementClicks(shortCode: string): Promise<void> {
+    await this.urlRepository.incrementClicks(shortCode);
+  }
 }
